d99f689bbf94dd1b7905f1454dc2660471f5207b
Semi <44993660+zhangbaijin@users.noreply.github.com>
2022-09-30 19:04:42 +0800

    Update bigresize.py

--- bigresize.py
+++ bigresize.py
@@ -6,5 +6,5 @@ for maindir, subdir,file_name_list in os.walk(path):
     for file_name in file_name_list:
         image=os.path.join(maindir,file_name) #获取每张图片的路径
         file=Image.open(image)
-        out=file.resize((256,256),Image.ANTIALIAS)  #以高质量修改图片尺寸为（400，48）
+        out=file.resize((640,480),Image.ANTIALIAS)  #以高质量修改图片尺寸为（400，48）
         out.save(image)                            #以同名保存到原路径
